588b38167caaa69ecbb1e193188b1b6d61e024b9
Thuria234naing <113816550+Thuria234naing@users.noreply.github.com>
2025-04-11 00:23:12 +0630

    donate mr notes a twt

--- pages/donate.tsx
+++ pages/donate.tsx
@@ -234,8 +234,8 @@ const DonatePage: React.FC = () => {
                     09765127445 <br />
                     Htet Yadanar Myo
                   </Text>
-                  <Text fontSize="sm" color="gray.800">
-                    Please paste this unique key in notes: <strong>{uniqueKey}</strong>
+                  <Text fontSize="lg" color="gray.800">
+                  Notes တွင်ကူးထည့်ပေးရန်: <strong>{uniqueKey}</strong>
                     <Button
                       variant="link"
                       leftIcon={<CopyIcon />}
